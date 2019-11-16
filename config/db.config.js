69dfc906024cfddb2aab2dceea29bbb959ae2c01
abhimanyu sikarwar <asikarwar007@gmail.com>
2019-11-16 22:49:01 +0530

    db

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: "mongodb://localhost:27017/houzzify"
+    url: "mongodb://locus:locus1@ds018839.mlab.com:18839/locus"
 }
   
 // url: "mongodb://houzzify:houzzify1@ds153556.mlab.com:53556/houzzify"
